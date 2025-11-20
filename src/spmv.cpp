14523845f2d9601240d513b59edc5468e6b5f444
ElPerenza <pev.cookie@gmail.com>
2025-11-20 15:29:35 +0100

    Change way the multiplication is benchmarked and save results to a csv

--- src/spmv.cpp
+++ src/spmv.cpp
@@ -10,6 +10,10 @@
 #include "benchmark.hpp"
 #include "matrix.hpp"
 
+#define CACHE_LINE_SIZE 64 // x86-64 CPUs have 64 bytes for cache line size
+#define BENCHMARK_RUNS 10
+#define BENCHMARK_WARMUP 1
+
 /// @brief Generate an integer array randomly filled with values between -100 and 100.
 /// @param size the size of the array
 /// @return the generated array
@@ -49,7 +53,7 @@ std::vector<double> generate_real_array(size_t size) {
 /// @return the result of the multiplication
 template<typename T> std::unique_ptr<T[]> matrix_vector_multiplication(csr_matrix<T>& matrix, std::vector<T>& array) {
     
-    alignas(64) T* result = new T[array.size()];
+    alignas(CACHE_LINE_SIZE) T* result = new T[array.size()];
 
     for(size_t i = 0; i < array.size(); i++) {
         long row_start = matrix.row_indices[i];
@@ -71,7 +75,7 @@ template<typename T> std::unique_ptr<T[]> matrix_vector_multiplication(csr_matri
 /// @return the result of the multiplication
 template<typename T> std::unique_ptr<T[]> matrix_vector_multiplication_parallel(csr_matrix<T>& matrix, std::vector<T>& array) {
     
-    alignas(64) T* result = new T[array.size()];
+    alignas(CACHE_LINE_SIZE) T* result = new T[array.size()];
 
     #pragma omp parallel for default(none) shared(result, matrix, array)
     for(size_t i = 0; i < array.size(); i++) {
@@ -87,17 +91,43 @@ template<typename T> std::unique_ptr<T[]> matrix_vector_multiplication_parallel(
     return std::unique_ptr<T[]>(result);
 }
 
+void print_results(benchmark_results results) {
+    std::cout << "RESULTS:\n";
+    std::cout << "Fastest: " << results.fastest_time << "ms\n";
+    std::cout << "Slowest: " << results.slowest_time << "ms\n";
+    std::cout << "Average: " << results.average_time << "ms\n";
+    std::cout << "90th percentile: " << results.ninetieth_percentile_time << "ms\n";
+    std::cout << "Run times: ";
+    for(long v : results.times) {
+        std::cout << v << "ms, ";
+    }
+    std::cout << "\n";
+}
+
+void write_results_to_file(std::string filename, benchmark_results results) {
+    std::ofstream output(filename);
+    output << "fastest,slowest,ninetieth\n";
+    output << results.fastest_time << "," << results.slowest_time << "," << results.ninetieth_percentile_time << "\n";
+    output.flush();
+    output.close();
+}
+
 int main(int argc, char* argv[]) {
 
-    if(argc == 1) {
-        std::cout << "USAGE: spmv <matrix-path>\n";
+    if(argc != 4) {
+        std::cout << "USAGE:" << argv[0] << "[sequential | parallel] <matrix-path> <results-output-path>\n";
         return 0;
     }
-    if(argc != 2) {
-        throw std::invalid_argument("No file path present.");
+
+    std::string execution_mode(argv[1]);
+    if(execution_mode != "sequential" && execution_mode != "parallel") {
+        std::cout << "Invalid execution mode (must be sequential or parallel): " << execution_mode << "\n";
+        return -1;
     }
 
-    std::ifstream file(argv[1]);
+    std::string matrix_path(argv[2]);
+    std::string results_path(argv[3]);
+    std::ifstream file(matrix_path);
     std::string header;
     std::getline(file, header);
     matrix_metadata metadata = identify_matrix(header);
@@ -112,37 +142,26 @@ int main(int argc, char* argv[]) {
             std::cout << "Generating vector...\n";
             std::vector<long> test_arr = generate_integer_array(m.n_rows);
 
-            std::cout << "Starting sequential benchmark...\n";
-            benchmark_results results = benchmark([&m, &test_arr]() { matrix_vector_multiplication(m, test_arr); }, 10, 1);
-            std::cout << "RESULTS:\n";
-            std::cout << "Fastest: " << results.fastest_time << "ms\n";
-            std::cout << "Slowest: " << results.slowest_time << "ms\n";
-            std::cout << "Average: " << results.average_time << "ms\n";
-            std::cout << "90th percentile: " << results.ninetieth_percentile_time << "ms\n";
-            std::cout << "Run times: ";
-            for(long v : results.times) {
-                std::cout << v << "ms, ";
+            std::function<void ()> benchmark_function;
+            if(execution_mode == "sequential") {
+                benchmark_function = [&m, &test_arr]() { 
+                    matrix_vector_multiplication(m, test_arr); 
+                };
+            } else if(execution_mode == "parallel") {
+                benchmark_function = [&m, &test_arr]() { 
+                    matrix_vector_multiplication_parallel(m, test_arr); 
+                };
+            } else {
+                throw std::logic_error("how?"); //in theory, impossible to reach since we already checked
             }
-            std::cout << "\n";
-
-            std::cout << "Starting parallel benchmark...\n";
-            benchmark_results results2 = benchmark([&m, &test_arr]() { matrix_vector_multiplication_parallel(m, test_arr); }, 10, 1);
-            std::cout << "RESULTS:\n";
-            std::cout << "Fastest: " << results2.fastest_time << "ms\n";
-            std::cout << "Slowest: " << results2.slowest_time << "ms\n";
-            std::cout << "Average: " << results2.average_time << "ms\n";
-            std::cout << "90th percentile: " << results2.ninetieth_percentile_time << "ms\n";
-            std::cout << "Run times: ";
-            for(long v : results2.times) {
-                std::cout << v << "ms, ";
-            }
-            std::cout << "\n";
-
-            double speedup = (results.ninetieth_percentile_time - results2.ninetieth_percentile_time) / ((double) results.ninetieth_percentile_time) * 100;
-            std::cout << "Speedup of parallel compared to sequential: " << speedup << "%\n";
-
+        
+            std::cout << "Starting benchmark...\n";
+            benchmark_results results = benchmark(benchmark_function, BENCHMARK_RUNS, BENCHMARK_WARMUP);
+            print_results(results);
+            write_results_to_file(results_path, results);
             break;
         }
+
         case field_type::real: {
             
             std::cout << "Loading matrix...\n";
@@ -152,35 +171,23 @@ int main(int argc, char* argv[]) {
             std::cout << "Generating vector...\n";
             std::vector<double> test_arr = generate_real_array(m.n_rows);
 
-            std::cout << "Starting sequential benchmark...\n";
-            benchmark_results results = benchmark([&m, &test_arr]() { matrix_vector_multiplication(m, test_arr); }, 10, 5);
-            std::cout << "\nRESULTS:\n";
-            std::cout << "Fastest: " << results.fastest_time << "ms\n";
-            std::cout << "Slowest: " << results.slowest_time << "ms\n";
-            std::cout << "Average: " << results.average_time << "ms\n";
-            std::cout << "90th percentile: " << results.ninetieth_percentile_time << "ms\n";
-            std::cout << "Run times: ";
-            for(long v : results.times) {
-                std::cout << v << "ms, ";
+            std::function<void ()> benchmark_function;
+            if(execution_mode == "sequential") {
+                benchmark_function = [&m, &test_arr]() { 
+                    matrix_vector_multiplication(m, test_arr); 
+                };
+            } else if(execution_mode == "parallel") {
+                benchmark_function = [&m, &test_arr]() { 
+                    matrix_vector_multiplication_parallel(m, test_arr); 
+                };
+            } else {
+                throw std::logic_error("how?"); //in theory, impossible to reach since we already checked
             }
-            std::cout << "\n";
-
-            std::cout << "Starting parallel benchmark...\n";
-            benchmark_results results2 = benchmark([&m, &test_arr]() { matrix_vector_multiplication_parallel(m, test_arr); }, 10, 5);
-            std::cout << "\nRESULTS:\n";
-            std::cout << "Fastest: " << results2.fastest_time << "ms\n";
-            std::cout << "Slowest: " << results2.slowest_time << "ms\n";
-            std::cout << "Average: " << results2.average_time << "ms\n";
-            std::cout << "90th percentile: " << results2.ninetieth_percentile_time << "ms\n";
-            std::cout << "Run times: ";
-            for(long v : results2.times) {
-                std::cout << v << "ms, ";
-            }
-            std::cout << "\n";
-
-            double speedup = (results.ninetieth_percentile_time - results2.ninetieth_percentile_time) / ((double) results.ninetieth_percentile_time) * 100;
-            std::cout << "Speedup of parallel compared to sequential: " << speedup << "%\n";
-
+        
+            std::cout << "Starting benchmark...\n";
+            benchmark_results results = benchmark(benchmark_function, BENCHMARK_RUNS, BENCHMARK_WARMUP);
+            print_results(results);
+            write_results_to_file(results_path, results);
             break;
         }
     }
